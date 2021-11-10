2dfe25760187ee5ea6f336662f7af7e9fe0070da
Genesisnbq <genesis.nbq@gmail.com>
2021-11-11 03:44:10 +0800

    Update: s2r subcommand

--- cmd/s2r.go
+++ cmd/s2r.go
@@ -1,11 +1,22 @@
 package cmd
 
-import "github.com/spf13/cobra"
-
-// swagger to restful Use:   "s2r",api
+import (
+	"fmt"
+	"github.com/spf13/cobra"
+	"os"
+	"os/exec"
+)
 
 func init() {
 	RootCmd.AddCommand(s2r)
+}
+
+func swagger2Restful(swaggerFilePath string) (err error) {
+	command := exec.Command("swagger", "serve", "-F=swagger", swaggerFilePath)
+	if err = command.Run(); err != nil {
+		fmt.Println(err.Error())
+	}
+	return err
 }
 
 var s2r = &(cobra.Command{
@@ -13,6 +24,16 @@ var s2r = &(cobra.Command{
 	Short: "swagger to restful api",
 	Long:  `swagger to restful api`,
 	Run: func(cmd *cobra.Command, args []string) {
-
+		switch {
+		case len(args) == 0:
+			fmt.Println("please input swagger file path...")
+		case len(args) == 1:
+			if err := swagger2Restful(args[0]); err != nil {
+				fmt.Println(err)
+				os.Exit(1)
+			}
+		default:
+			fmt.Println("too many args...")
+		}
 	},
 })
